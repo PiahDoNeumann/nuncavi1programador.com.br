f3ca28a221e374d67ba36ad1d55cf45bbaef5ebd
Valdecir Neumann <56264244+PiahDoNeumann@users.noreply.github.com>
2022-01-22 15:23:56 -0300

    First commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>Website {{host}}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  data () {
+      return {
+        currentUrl: location.toString(),
+        host: location.host
+      }
+    }
+}
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    border-bottom: 1px solid #ccc;
+    padding: .1rem;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
